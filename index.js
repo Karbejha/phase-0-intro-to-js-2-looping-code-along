f50aa46c64082af70c8e852fbe956a33b6936436
Mohamad Karbejha <78583194+Karbejha@users.noreply.github.com>
2023-10-01 23:47:48 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const gifts = ["Guadalupe", "Ollie", "Aki"];
+let messages=[...gifts] ;
+function writeCards(gifts,surprise) {
+  for (let i = 0; i < gifts.length; i++) {
+    messages[i]=`Thank you, ${gifts[i]}, for the wonderful ${surprise} gift!`;
+}
+
+  return messages;
+}
+
+writeCards(gifts);
+
+function countDown(number){
+    let countup = number;
+    while (countup >= 0) {
+      console.log(countup--);
+    }
+}
+
+countDown(10);
